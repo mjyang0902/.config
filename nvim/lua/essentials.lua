24b1f8d7bbf364c73c777e578c92e287447a4e6a
mjyang0902 <adam.mj.yang+ntu@gmail.com>
2024-02-26 23:57:17 +0800

    basic config

---
+++ nvim/lua/essentials.lua
@@ -0,0 +1,62 @@
+local option = vim.opt
+local buffer = vim.b
+local global = vim.g
+
+-- Global Settings --
+option.showmode = false
+option.backspace = { "indent", "eol", "start" }
+option.tabstop = 4
+option.shiftwidth = 4
+option.expandtab = true
+option.shiftround = true
+option.autoindent = true
+option.smartindent = true
+option.number = true
+option.relativenumber = true
+option.wildmenu = true
+option.hlsearch = false
+option.ignorecase = true
+option.smartcase = true
+option.completeopt = { "menu", "menuone"}
+option.cursorline = true
+option.termguicolors = true
+option.signcolumn = "yes"
+option.autoread = true
+option.title = true
+option.swapfile = false
+option.backup = false
+option.updatetime = 50
+option.mouse = "a"
+option.undofile = true
+option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
+option.exrc = true
+option.wrap = false
+option.splitright = true
+
+-- Buffer Settings --
+buffer.fileencoding = "utf-8"
+
+-- Global Settings --
+global.mapleader = " "
+
+-- Key Mappings --
+vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
+vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
+vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
+vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
+
+vim.keymap.set("n", "<Tab>", "<cmd>bNext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>bc", "<cmd>bd>CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set({ "v", "n" } , "<leader>y", "\"+y")
+
+vim.keymap.set({ "i", "v" }, "jk", "<ESC>")
+vim.keymap.set("n", "<leader>w", ":w", { silent = true })
+vim.keymap.set("n", "<leader>q", ":q", { silent = true })
+vim.keymap.set("n", "<leader>wq", ":wq", { silent = true })
+vim.keymap.set("n", "<leader>w!", ":w!", { silent = true })
+vim.keymap.set("n", "<leader>q!", ":q!", { silent = true })
+vim.keymap.set("n", "<leader>wq!", ":wq!", { silent = true })

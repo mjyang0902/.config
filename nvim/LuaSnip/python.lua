881f87c92cc9e95295d302bc0259cc499d2c660a
mjyang0902 <adam.mj.yang+ntu@gmail.com>
2024-03-27 20:27:20 +0800

    write python snip

--- nvim/LuaSnip/python.lua
+++ nvim/LuaSnip/python.lua
@@ -26,8 +26,60 @@ local ms = ls.multi_snippet
 local k = require("luasnip.nodes.key_indexer").new_key
 
 return {
-    s("def", {
-    t"def ", i(1, "name"), t"(", i(2, "arg"), t({"):", "\t"}),
+    s({ trig = "def", dscr = "Code snippet for function definition."}, {
+        t"def ", i(1, "name"), t"(", i(2, "arg"), t({"):", "\t"}), i(3, "pass")
+    }),
+    s({ trig = "class", dscr = "Code snippet for a class definition."}, {
+        t("class "), i(1, "Classname"), t("("), i(2, "object"), t({"): ", "\t"}),
         i(3, "pass")
     }),
+    s({ trig = "if", dscr = "Code snippet for the if statement." }, {
+        t"if ", i(1, "condition"), t({":", "\t"}), i(2, "pass")
+    }),
+    s({ trig = "if/else", dscr = "Code snippet for an if statement with else" }, {
+        t"if ", i(1, "condition"), t({":", "\t"}), i(2, "pass"),
+        t({"", "else:", "\t"}), i(3, "pass")
+    }),
+    s({ trig = "elif", dscr = "Code snippet for an elif" }, {
+        t"elif ", i(1, "expression"), t({":", "\t"}), i(2, "pass")
+    }),
+    s({ trig = "else", dscr = "Code snippet for an else" }, {
+        t({"else:","\t"}), i(1, "pass")
+    }),
+    s({ trig = "for", dscr = "Code snippet to create a for loop structure." }, {
+        t"for ", i(1, "value"), t" in ", i(2, "iterable"), t({":", "\t"}), i(3, "pass")
+    }),
+    s({ trig = "for/else", dscr = "Code snippet for a for loop with else" }, {
+        t"for ", i(1, "target_list"), t" in ", i(2, "expression_list"), t({":", "\t"}),
+        i(3, "pass"), t({"", "else:", "\t"}), i(4, "pass")
+    }),
+    s({ trig = "while", dscr = "Code snippet to create a while loop structure." }, {
+        t"while ", i(1, "condition"), t({":", "\t"}), i(2, "pass")
+    }),
+    s({ trig = "while/else", dscr = "Code snippet for a while loop with else" }, {
+        t"while ", i(1, "expression"), t({":", "\t"}), i(2, "pass"),
+        t({"", "else:", "\t"}), i(3, "pass")
+    }),
+
+    parse({ trig = "try", dscr = "Code Snippet for a try and except blocks." },
+        "try:\n\t${1:pass}\nexcept ${2:Exception} as ${3:e}:\n\t${4:raise $3}$0"),
+
+    parse({ trig = "tryef", dscr = "Code Snippet for a try/except/finally with else statement." },
+        "try:\n\t${1:pass}\nexcept${2: ${3:Exception} as ${4:e}}:\n\t${5:raise}\nelse:\n\t${6:pass}\nfinally:\n\t${7:pass}$0"),
+
+    parse({ trig = "trye", dscr = "Code Snippet for a try/except with else statement." },
+        "try:\n\t${1:pass}\nexcept ${2:Exception} as ${3:e}:\n\t${4:raise $3}\nelse:\n\t${5:pass}$0"),
+
+    parse({ trig = "tryf", dscr = "Code Snippet for a try/except/finally." },
+        "try:\n\t${1:pass}\nexcept ${2:Exception} as ${3:e}:\n\t${4:raise $3}\nfinally:\n\t${5:pass}$0"),
+
+    parse({ trig = "with", dscr = "Code snippet for a with statement" },
+        "with ${1:expression} as ${2:target}:\n\t${3:pass}"),
+
+    parse({ trig = "s", dscr = "Shortend snippet to reference the self property in an object." },
+        "self.$0"),
+
+    parse({ trig = "ifmain", dscr = "Create implicitly all the code at the top level using the __name__ special variable." },
+        "if __name__ == \"__main__\":\n\t${1:main()}$0"),
 }
+

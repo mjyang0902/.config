dbc3fe848e2c1636d4ad9c6fe319f69313b3ea89
mjyang0902 <adam.mj.yang+ntu@gmail.com>
2024-04-04 14:16:22 +0800

    latex support

---
+++ nvim/lua/plugins/vimtex.lua
@@ -0,0 +1,22 @@
+return {
+    "lervag/vimtex",
+    dependencies = {
+        "KeitaNakamura/tex-conceal.vim",
+    },
+    init = function()
+
+        vim.g.tex_flavor = 'latex' 
+
+        -- use zathura as the default pdf reviewer
+        vim.g.vimtex_view_general_viewer = 'zathura'
+--        vim.g.vimtex_view_method = 'zathura'
+
+        -- show the compiler hint
+        vim.g.vimtex_compiler_progname = 'nvr'
+        vim.g.vimtex_quick_mode = 0 
+        
+        -- hide the last two lines
+        -- set conceallevel = 1
+        vim.g.tex_conceal = 'abdmg'
+    end,
+}
